ab535b853cc44fb61ffeacc4bb14b55927577cec
Nezhadian <Nezhadian@XIGNITE-POOYA-2>
2017-06-25 17:40:05 -0700

    Add project files.

---
+++ ExperienceWithAkka.Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Configuration;
+using ExperienceWithAkka.Shared;
+
+namespace ExperienceWithAkka.Client
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+
+            var config = ConfigurationFactory.ParseString(@"
+                akka {
+                    actor {
+                        provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                    }
+                    remote {
+                        helios.tcp {
+                            port = 8090
+                            hostname = localhost
+                        }
+                    }
+                }
+            ");
+
+            using (var system = ActorSystem.Create("MyClient", config))
+            {
+                //get a reference to the remote actor
+                var greeter = system
+                    .ActorSelection("akka.tcp://MyServer@localhost:8080/user/greeter");
+                //send a message to the remote actor
+                while (true)
+                {
+                    greeter.Tell(new Greet(Console.ReadLine()));
+                }
+            }
+        }
+    }
+}
---
+++ ExperienceWithAkka.Service/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace ExperienceWithAkka.Service
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+
+            var config = ConfigurationFactory.ParseString(@"
+                akka {
+                    actor {
+                        provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                    }
+
+                    remote {
+                        helios.tcp {
+                            port = 8080
+                            hostname = localhost
+                        }
+                    }
+                }
+            ");
+
+            using (var system = ActorSystem.Create("MyServer", config))
+            {
+                system.ActorOf<GreetingActor>("greeter");
+                Console.ReadKey();
+            }
+        }
+    }
+}
